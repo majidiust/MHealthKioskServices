506a921a4f600b9021a7d44fea420ae6a03b0077
majid.sadeghi.alavijeh@gmail.com <majid.sadeghi.alavijeh@gmail.com>
2014-10-06 15:42:15 +0330

    Migrate from local repository

---
+++ IMHealthKioskService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MKioskService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMHealthKioskService" in both code and config file together.
+    [ServiceContract]
+    public interface IMHealthKioskService
+    {
+        [OperationContract]
+        String AddVisit(String DeviceInfo, String NationalID, String MobileNumber, String Systolic, String Diastolic, String SPO2Percentage, String SPO2PulseRate);
+    }
+}
---
+++ MHealthKioskService.svc.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Net;
+using System.IO;
+namespace MKioskService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MHealthKioskService" in code, svc and config file together.
+    public class MHealthKioskService : IMHealthKioskService
+    {
+        public String AddVisit(String DeviceInfo, String NationalID, String MobileNumber, String Systolic, String Diastolic, String SPO2Percentage, String SPO2PulseRate)
+        {
+            try
+            {
+                DatabaseDataContext db = new DatabaseDataContext();
+                VisitDescribtion newVisit = new VisitDescribtion();
+                newVisit.Confirmed = false;
+                newVisit.KioskSerial = DeviceInfo;
+                newVisit.MobileNumber = MobileNumber;
+                newVisit.NationalityCode = NationalID;
+                newVisit.NIBPDiastol = Diastolic;
+                newVisit.NIBPSystol = Systolic;
+                newVisit.SPO2 = SPO2Percentage;
+                newVisit.RR = SPO2PulseRate;
+                newVisit.SendSMS = false;
+                db.VisitDescribtions.InsertOnSubmit(newVisit);
+                db.SubmitChanges();
+
+                //TODO : Send to health-cloud.ir server via json
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://5.200.78.14:80/api/v1/kiosk_services");
+                httpWebRequest.ContentType = "text/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    string json = "{\"phone\":\"" + MobileNumber + "\"," +
+                                  "\"nibp_sys\":\"" + Systolic + "\"," + 
+                                  "\"nibp_dia\":\"" + Diastolic + "\"," +
+                                  "\"spo2\":\"" + SPO2Percentage + "\","  +
+                                  "\"heart_rate\":\"" + SPO2PulseRate + "\"," +
+                                  "\"national_id\":\"" + NationalID + "\"," +
+                                  "\"device_id\":\"" + DeviceInfo + "\"," + "}";
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+
+                    var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                    {
+                        var result = streamReader.ReadToEnd();
+                    }
+
+                    return json;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return ex.Message;
+            }
+        }
+    }
+}
